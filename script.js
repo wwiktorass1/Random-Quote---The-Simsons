2d0c56e25d592d9a6b88acbcfdc71afea4a82752
Viktoras <47609957+wwiktorass1@users.noreply.github.com>
2019-06-04 15:20:33 +0300

    Update javaScript code
    add proxy server  for Cors.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("year").innerHTML = new Date().getFullYear() +" m.";
 
 let btn = document.getElementById("randomm");
 btn.addEventListener("click", function () {
-    //let proxyForCors ="https://cors-anywhere.herokuapp.com/";
-    $.getJSON( "https://thesimpsonsquoteapi.glitch.me/quotes", function( data ) {
+    let proxyForCors ="https://cors-anywhere.herokuapp.com/";
+    $.getJSON( proxyForCor +"https://thesimpsonsquoteapi.glitch.me/quotes", function( data ) {
         console.log(data);
        let quote = (data[0].quote);
         console.log(quote);
