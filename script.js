817a2c934833124214cc016873344a155eb68b65
Viktoras <47609957+wwiktorass1@users.noreply.github.com>
2019-06-04 15:16:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("year").innerHTML = new Date().getFullYear() +" m.";
+
+let btn = document.getElementById("randomm");
+btn.addEventListener("click", function () {
+    //let proxyForCors ="https://cors-anywhere.herokuapp.com/";
+    $.getJSON( "https://thesimpsonsquoteapi.glitch.me/quotes", function( data ) {
+        console.log(data);
+       let quote = (data[0].quote);
+        console.log(quote);
+         let imgSrc = (data[0].image);
+        console.log(imgSrc);
+
+             let character= (data[0].character);
+        console.log(character);
+
+        document.getElementById("citation").innerHTML = quote;
+         document.getElementById("character").innerHTML = character;
+        document.getElementById("right").src = imgSrc;
+    });
+})
+
